7ab6afe93f9ec3dfc10ebd6cd33989bbfdd3f1a5
Randy López <77851376+ralph19exe@users.noreply.github.com>
2021-09-28 20:26:35 -0600

    Delete tests-nodejs directory

--- tests-nodejs/tests/calculos.resta.spec.js
+++
@@ -1,9 +0,0 @@
-const {resta} = require("../controllers/calculos");
-
-describe('Pruebas Funcion Resta', () => {
-    
-    test('Debería restar dos números ', () => {
-        const actual = resta(3,2);
-        expect(actual).toBe(1);
-    })  
-})
